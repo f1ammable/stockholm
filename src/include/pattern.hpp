4154aca189201256f85a04aa458aa1fad9b4c701
Gabriel <d9rect@pm.me>
2025-05-05 00:17:20 +0100

    add basic capture group implementation

--- src/include/pattern.hpp
+++ src/include/pattern.hpp
@@ -11,46 +11,52 @@
 
 namespace stockholm {
 
-class BasePattern {
+class Pattern {
  private:
   detail::Yarn<> m_pattern;
 
  public:
   template <std::size_t N>
-  explicit consteval BasePattern(const char (&str)[N]) : m_pattern(str) {}
+  explicit consteval Pattern(const char (&str)[N]) : m_pattern(str) {}
 
-  explicit consteval BasePattern(char c) : m_pattern(c) {}
+  explicit consteval Pattern(char c) : m_pattern(c) {}
 
-  explicit consteval BasePattern(const std::string& s)
-      : m_pattern(std::move(s)) {}
+  explicit consteval Pattern(const std::string& s) : m_pattern(std::move(s)) {}
 
   [[nodiscard]] consteval std::string_view str() const {
     return this->m_pattern.view();
   }
 
-  [[nodiscard]] consteval BasePattern OneOrMore(std::string_view str) {
+  [[nodiscard]] consteval Pattern OneOrMore(std::string_view str) {
     m_pattern.append(detail::constexpr_fmt(FMT_COMPILE("[{}]+"), str));
     return *this;
   }
 
-  [[nodiscard]] consteval BasePattern One(std::string_view str) {
+  [[nodiscard]] consteval Pattern One(std::string_view str) {
     m_pattern.append(detail::constexpr_fmt(FMT_COMPILE("[{}]"), str));
+    return *this;
+  }
+
+  [[nodiscard]] consteval Pattern Group(const Pattern& pattern) {
+    m_pattern.append(detail::constexpr_fmt(FMT_COMPILE("({})"), pattern.str()));
     return *this;
   }
 };
 }  // namespace stockholm
 
-[[nodiscard]] consteval std::string Capture(
-    const stockholm::BasePattern& pattern) {
+[[nodiscard]] consteval std::string Capture(const stockholm::Pattern& pattern) {
   return std::string(pattern.str());
 }
 
-// [[nodiscard]] consteval stockholm::BasePattern Group(const
-// stockholm::BasePattern& pattern) {}
+[[nodiscard]] consteval stockholm::Pattern Group(
+    const stockholm::Pattern& pattern) {
+  return stockholm::Pattern(
+      stockholm::detail::constexpr_fmt(FMT_COMPILE("({})"), pattern.str()));
+}
 
 template <std::size_t N>
-[[nodiscard]] consteval stockholm::BasePattern OneOrMore(const char (&str)[N]) {
-  return stockholm::BasePattern(
+[[nodiscard]] consteval stockholm::Pattern OneOrMore(const char (&str)[N]) {
+  return stockholm::Pattern(
       stockholm::detail::constexpr_fmt(FMT_COMPILE("[{}]+"), str));
 }
 #endif  // !STOCKHOLM_PATTERN_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "include/pattern.hpp"
 
 int main(int argc, char *argv[]) {
-  constexpr auto x = Capture(OneOrMore("b").OneOrMore("@").One("b"));
+  constexpr auto x = Capture(Group(OneOrMore("b")).Group(OneOrMore("@")));
   std::cout << x << std::endl;
   return 0;
 }
